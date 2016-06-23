d44ae764a3f161f0ac41e44abe5c35cf7649817b
Dmitriy Shekhovtsov <valorkin@gmail.com>
2016-06-23 16:24:46 +0300

    s

---
+++ .ng2-config.js
@@ -0,0 +1,28 @@
+/* eslint no-var:0 */
+'use strict';
+var pkg = require('./package.json');
+
+module.exports = {
+  // metadata
+  title: pkg.description,
+  baseUrl: '/',
+  // root folder name
+  src: 'demo',
+  dist: 'demo-build',
+  htmlIndexes: ['index.html', 'index-bs4.html'],
+  // webpack entry
+  entry: {
+    polyfills: './demo/polyfills.ts',
+    vendor: './demo/vendor.ts',
+    main: './demo/index.ts'
+  },
+  commonChunks: {
+    name: ['polyfills', 'vendor'].reverse()
+  },
+  // webpack alias
+  alias: {},
+  copy: [
+    {from: 'demo/favicon.ico', to: 'favicon.ico'},
+    {from: 'demo/assets', to: 'assets'}
+  ]
+};
